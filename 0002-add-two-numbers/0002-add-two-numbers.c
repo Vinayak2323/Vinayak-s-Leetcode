b3539a4c1e7b3374de9b2c7e2132fa357a5499e2
Vinayak Pathak <vrpathak102@gmail.com>
2026-08-26 21:40:10 +0530

    Time: 7 ms (5.92%), Space: 13.4 MB (56.82%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.c
@@ -0,0 +1,83 @@
+#include<stdlib.h>
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode*ptr1=l1;
+    struct ListNode*ptr2=l2;
+    struct ListNode*head=NULL;
+    struct ListNode*ptr3=NULL;
+    head=malloc(sizeof(struct ListNode));
+    ptr3=head;
+    ptr3->next=NULL;
+    if(ptr1==NULL&&ptr2==NULL){
+        printf("invalid");
+        return 0;
+    }
+    if(ptr1==NULL){
+        return ptr2;
+    }
+    if(ptr2==NULL){
+        return ptr1;
+    }
+    ptr3->val=ptr1->val+ptr2->val;
+    ptr1=ptr1->next;
+    ptr2=ptr2->next;
+    int carry=0;
+    if(ptr3->val>9){
+            ptr3->val=ptr3->val%10;
+            carry=1;
+        }
+    while(ptr1!=NULL&&ptr2!=NULL){
+        
+        ptr3->next=malloc(sizeof(struct ListNode));
+        ptr3=ptr3->next;
+        ptr3->next=NULL;
+        ptr3->val=ptr1->val+ptr2->val+carry;
+        carry=0;
+        if(ptr3->val>9){
+            ptr3->val=ptr3->val%10;
+            carry=1;
+        }
+        ptr1=ptr1->next;
+        ptr2=ptr2->next;
+        //ptr3=ptr3->next;
+        //ptr3->next=malloc(sizeof(struct ListNode));
+    }
+    while(ptr1!=NULL){
+        ptr3->next=malloc(sizeof(struct ListNode));
+        ptr3=ptr3->next;
+        ptr3->next=NULL;
+        ptr3->val=ptr1->val+carry;
+        carry=0;
+        if(ptr3->val>9){
+            ptr3->val=ptr3->val%10;
+            carry=1;
+        }
+        ptr1=ptr1->next;
+    }
+    while(ptr2!=NULL){
+        ptr3->next=malloc(sizeof(struct ListNode));
+        ptr3=ptr3->next;
+        ptr3->next=NULL;
+        ptr3->val=ptr2->val+carry;
+        carry=0;
+        if(ptr3->val>9){
+            ptr3->val=ptr3->val%10;
+            carry=1;
+        }
+        ptr2=ptr2->next;
+    }
+    if(carry==1){
+        ptr3->next=malloc(sizeof(struct ListNode));
+        ptr3=ptr3->next;
+        ptr3->next=NULL;
+        ptr3->val=1;
+        
+    }
+    return head;
+}
